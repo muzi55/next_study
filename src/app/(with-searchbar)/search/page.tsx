3f39447340537ceb7e2f308f78840010ec731c68
muzi5@kakao.com <muzi5@kakao.com>
2024-10-08 15:53:31 +0900

    레이아웃 및 라우트 그룹
    layout은 중첩 될 수 있으며, 패스뒤에 있는 경우 모든 페이지에 레이아웃으로 적용된다
    route group은 라우팅에 영향을 주지 않으며, 레이아웃을 동일하게 유지하는데 사용 가능하다

---
+++ src/app/(with-searchbar)/search/page.tsx
@@ -0,0 +1,10 @@
+export default function Search({
+  searchParams,
+}: {
+  searchParams: {
+    q?: string;
+  };
+}) {
+  console.log("searchParams", searchParams);
+  return <div>search 페이지 [{searchParams.q}]</div>;
+}
--- src/app/book/[id]/page.tsx
+++ src/app/book/[id]/page.tsx
@@ -1,3 +1,16 @@
+// Generating Static Params
+// book/55
+// [id] => '55'
+
+// Catch-all Segments
+// book/55/66/77
+// [...id] => ['55', '66', '77']
+
+// Optional Catch-all Segments
+// book
+// [[...id]] => 에러를 뱉지 않음
+// book/55/66/77
+// [[...id]] => ['55', '66', '77']
 export default function Book({
   params,
 }: {
