b2f7789ec216470b7ce3e3060b000b2e79f46d67
muzi5@kakao.com <muzi5@kakao.com>
2024-10-09 20:38:38 +0900

    next:{revalidate:3}
    - 특정 시간을 주기로 캐시를 업데이트함
    - 마치 page router의 isr방식과 유사함
    
    플로우는 force-cache와 비슷하지만
    처음 요청을 set하게되며 지정한 시간이 지나면 stale 상태가 된다
    stale 상태일떄 해당 요청을 먼저 보여준 이후, 다시 요청을하여 데이터를 최신화한다

--- src/components/RecoBooks.tsx
+++ src/components/RecoBooks.tsx
@@ -3,14 +3,18 @@ import BookItem from "./book-item";
 import type { BookData } from "@/types";
 
 export default async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, { cache: "force-cache" });
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, {
+    next: {
+      revalidate: 3,
+    },
+  });
   if (!response.ok) {
     return <div>해당 요청에 오류가 발생했습니다.</div>;
   }
   const allBooks: BookData[] = await response.json();
   return (
     <section>
-      <h3>등록된 모든 도서</h3>
+      <h3>지금 추천하는 도서</h3>
       {allBooks.map((book) => (
         <BookItem key={book.id} {...book} />
       ))}
