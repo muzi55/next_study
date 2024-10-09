6d77cc8c910ac8242e2bbc5e0b921ffe4751dd5c
muzi5@kakao.com <muzi5@kakao.com>
2024-10-09 19:32:49 +0900

    force-cache
    요청의 결과를 무조건 캐싱함
    한번 호출된 이후에는 다시 호출되지 않음
    캐싱된 정보를 보기위해서는 .next폴더의 cache 폴더안에 내용이 있음

--- src/components/RecoBooks.tsx
+++ src/components/RecoBooks.tsx
@@ -3,7 +3,7 @@ import BookItem from "./book-item";
 import type { BookData } from "@/types";
 
 export default async function RecoBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/random`, { cache: "force-cache" });
   if (!response.ok) {
     return <div>해당 요청에 오류가 발생했습니다.</div>;
   }
