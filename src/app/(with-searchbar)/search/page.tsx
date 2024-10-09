ad230c4d5420c9c1caa6e73fbdcac63f1ad107a3
muzi5@kakao.com <muzi5@kakao.com>
2024-10-09 19:07:39 +0900

    서치 페이지와 디테일 페이지
    서치 페이지
    - searchParams를 사용한 ssr 방식 페칭
    디테일 페이지
    - params를 사용한 ssr 방식 페칭

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,13 +1,21 @@
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import type { BookData } from "@/types";
 
-export default function Page({
-  searchParams,
-}: {
+interface ISearchParams {
   searchParams: {
     q?: string;
   };
-}) {
+}
+
+export default async function Page({ searchParams }: ISearchParams) {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`);
+  if (!response.ok) {
+    return <div>해당 요청에 오류가 발생했습니다.</div>;
+  }
+
+  const books: BookData[] = await response.json();
+
   return (
     <div>
       {books.map((book) => (
