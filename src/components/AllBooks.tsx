44d62a46ec8bc44986c2a747b56ecb61a20bd3ad
muzi5@kakao.com <muzi5@kakao.com>
2024-10-09 19:26:34 +0900

    데이터 캐싱 및 no-store
    next는 fetch를 활용하여 데이터를 next서버에 보관하는 기능이다.
    영구적으로 보관하거나, 특정 시간을 주기로 갱신 시키는 것도 가능하다.
    불필요한 요청을 줄여 웹 서비스의 성능을 향상 시킬 수 있다.
    fetch(`url`, {cache: option} 으로 사용한다.(axios등의 http 라이브러리에서는 캐싱을 따로 설정해줘야한다.
    next에서 fetch는 일반적인 fetch가 아닌, next에서 기능을 덧붙힌 것이다.
    
     no-store
    -데이터를 패칭하지 않으며, 데이터를 캐싱하지 않는 옵션값이다.
    
    여담으로 next14버전 이후로는 기본값으로 데이터를 캐싱하지 않는다.

--- src/components/AllBooks.tsx
+++ src/components/AllBooks.tsx
@@ -3,7 +3,7 @@ import BookItem from "./book-item";
 import type { BookData } from "@/types";
 
 export default async function AllBooks() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`, { cache: "no-store" });
   if (!response.ok) {
     return <div>해당 요청에 오류가 발생했습니다.</div>;
   }
