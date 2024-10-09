2f07883ce32c74eabe33d2b039368743d3142e12
muzi5@kakao.com <muzi5@kakao.com>
2024-10-09 18:54:59 +0900

    데이터 패칭
    next에서 ssr 방식으로 데이터를 패칭하기 위해서는 함수 선언 앞에 async를 붙혀야한다.
    next 에서 env를 사용할때 NEXT_PUBLIC 을 사용해야 서버컴포넌트와 클라이언트 컴포넌트 모두 사용 가능하게 사용할 수 있다.
    NEXT_PUBLIC 을 사용하지 않으면 클라이언트 컴포넌트에서는 사용이 불가능하다.

---
+++ src/components/AllBooks.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import BookItem from "./book-item";
+import type { BookData } from "@/types";
+
+export default async function AllBooks() {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`);
+  if (!response.ok) {
+    return <div>해당 요청에 오류가 발생했습니다.</div>;
+  }
+
+  const allBooks: BookData[] = await response.json();
+  return (
+    <section>
+      <h3>등록된 모든 도서</h3>
+      {allBooks.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </section>
+  );
+}
