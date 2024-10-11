f3d676842fe2e525e17d6c12b8919ce8b78c0b5b
muzi5@kakao.com <muzi5@kakao.com>
2024-10-11 14:52:33 +0900

    서스펜스
    서스펜스는 미완성의 뜻을 갖고 있으며 기존 loading 파일이 갖고 있던 단점을 해결할 수 있다.
    
    한번 로딩을 끝낸 페이지의 쿼리스트링이 바뀐다 해도 loading이 다시 출력되지 않는 문제 역시 서스펜스를  사용하여 해결 할  수 있다.
    
    Suspense의 key 속성을 추가해줌으로 일처리 가능하다

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -2,19 +2,13 @@ import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 import type { BookData } from "@/types";
 import { delay } from "@/util/delay";
+import { Suspense } from "react";
 
-// export const dynamic = "error";
-interface ISearchParams {
-  searchParams: {
-    q?: string;
-  };
-}
-
-export default async function Page({ searchParams }: ISearchParams) {
+async function SearchResult({ q }: { q: string }) {
   await delay(1500);
   // 해당 props를 받아서 동적으로 항상 값을 보여줘야하는 이 컴포넌트는
   // 데이터 캐시 최적화를 통해 성능을 향상 시킬 수 있다.
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`, {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${q}`, {
     cache: "force-cache",
   });
   if (!response.ok) {
@@ -29,5 +23,20 @@ export default async function Page({ searchParams }: ISearchParams) {
         <BookItem key={book.id} {...book} />
       ))}
     </div>
+  );
+}
+
+// export const dynamic = "error";
+interface ISearchParams {
+  searchParams: {
+    q?: string;
+  };
+}
+
+export default function Page({ searchParams }: ISearchParams) {
+  return (
+    <Suspense key={searchParams.q || ""} fallback={<p>loading ...</p>}>
+      <SearchResult q={searchParams.q || ""} />;
+    </Suspense>
   );
 }
