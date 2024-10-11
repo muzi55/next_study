7e768fd874beff6df6e3786d627f7eb05ead1f80
muzi5@kakao.com <muzi5@kakao.com>
2024-10-11 14:26:47 +0900

    스트리밍
    스트리밍은 특정 페이지를 렌더링 할 때 비동기 작업이 없는 컴포넌트 부터 즉시 넘겨준 이후, 비교적 오래 걸리는 컴포넌트의 렌더링 결과를 뒤늦게 보여주는게 스트리밍이다.
    
    쉽게 생각해 식당에서 메인메뉴를 시켰을때 밑반찬이 오는거라고 생각하면 된다.
    
    delay가 발생하더라고 전체 페이지의 렌더링을 늦추지 않는다.
    
    주의점
    - 하위 모든 경로에 스트리밍이 적용된다. /search 기준으로
    /search, /search/setting, ... 등 하위 요소에 모든 스트리밍이 적용된다

--- src/app/(with-searchbar)/search/page.tsx
+++ src/app/(with-searchbar)/search/page.tsx
@@ -1,6 +1,7 @@
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
 import type { BookData } from "@/types";
+import { delay } from "@/util/delay";
 
 // export const dynamic = "error";
 interface ISearchParams {
@@ -10,6 +11,7 @@ interface ISearchParams {
 }
 
 export default async function Page({ searchParams }: ISearchParams) {
+  await delay(1500);
   // 해당 props를 받아서 동적으로 항상 값을 보여줘야하는 이 컴포넌트는
   // 데이터 캐시 최적화를 통해 성능을 향상 시킬 수 있다.
   const response = await fetch(`${process.env.NEXT_PUBLIC_API_SERVER_URL}/book/search?q=${searchParams.q}`, {
