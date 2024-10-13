eaea32f6a01aab66ac14e678f17b6af296e9a3e7
muzi5@kakao.com <muzi5@kakao.com>
2024-10-13 19:18:17 +0900

    요청 재검증 revalidate
    revalidatePath 를 사용하여 서버 액션에서 사용할 수 있다.(서버 컴포넌트, 서버 액션)
    클라이언트 컴포넌트에서 호출 불가능하며 경로의 모든 캐시를 무효화한다. - 이후 다시 로드함
    풀 라우트 캐시와, 데이터 캐시도 무력화 되며, 다시 요청 이후 데이터 캐시는 set, 다음번 요청 끝에서 풀 라우트 캐시도 다시 set하게 된다.
    
    풀라우트 캐시를 삭제, 무효화 하며 새로 생성된 페이지를 풀라우트 캐시에 저장하지 않는다.
    새로 고침하고 다시 접속하게 된다면 캐시된 데이터를 사용할 수 없기에 넥스트 서버측에서 실시간으로 다이나믹하게 생성하여 보내준 이후 풀라우트 캐시에 저장한다.
    
    쉽게 얘기해 빌드시 풀라우트 캐시와 데이터 캐시에 저장된 값이, revalidatePath 메서드가 실행이되면 모두 사라지게된다.
    그 이후 각종 요청을 데이터 캐시에 저장한 이후, 다음번 요청이 왔을때 데이터 캐시 된 값을 사용하며, 풀라우트 캐시에 SET하게 되는것이다.
    
    이렇게 동작하는 이유는 항상 최신의 데이터를 보증하기 위함이다.

--- src/actions/createReview.ts
+++ src/actions/createReview.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { revalidatePath } from "next/cache";
+
 export const createReviewAction = async (formData: FormData) => {
   const bookId = formData.get("bookId")?.toString();
   // 서버 액션으로 설정이 됨
@@ -17,6 +19,10 @@ export const createReviewAction = async (formData: FormData) => {
       body: JSON.stringify({ content, author, bookId }),
     });
     console.log(res.status);
+
+    // 넥스트 서버가 인수로 전달한 경로를 다시 생성하게됨
+    // 해당 경로의 모든 api를 재요청 하게됨
+    revalidatePath(`/book/${bookId}`);
   } catch (error) {
     console.error(error);
     return;
